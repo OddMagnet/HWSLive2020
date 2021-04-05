ba05e1ecf1812e6e95c5d21038fe87ec8ed35471
OddMagnet <mibruenen@gmail.com>
2021-04-05 15:11:10 +0200

    Initial commit

---
+++ Journeys/Journeys/JourneysApp.swift
@@ -0,0 +1,17 @@
+//
+//  JourneysApp.swift
+//  Journeys
+//
+//  Created by Michael Brünen on 05.04.21.
+//
+
+import SwiftUI
+
+@main
+struct JourneysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # HWSLive2020
 Coding along with the HWSLive2020 Videos
+
+## Journeys
+- Lazy Stacks
+- Integrating Maps in SwiftUI
+-
