47ce5f1aab01d6830319a26d6151eccbbb4171a0
OddMagnet <mibruenen@gmail.com>
2021-04-09 16:30:23 +0200

    Add example solution for grids

--- Journeys/Journeys/Views/PicksView.swift
+++ Journeys/Journeys/Views/PicksView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct PicksView: View {
     @EnvironmentObject var locations: Locations
 
+    let columns = [
+        GridItem(.adaptive(minimum: 180))
+    ]
+
     var body: some View {
         // Example solution for Paging TabView
         ScrollView {
@@ -27,9 +31,45 @@ struct PicksView: View {
             .frame(height: 300)
             .tabViewStyle(PageTabViewStyle())
             .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+
+            // Example solution for Grids
+            LazyVGrid(columns: columns, alignment: .center, spacing: 0) {
+                ForEach(locations.places) { place in
+                    NavigationLink(destination: DiscoverView(location: place)) {
+                        Place(location: place)
+                    }
+                }
+            }
         }
         .background(Color(white: 0.95))
         .navigationTitle("Our Top Picks")
+    }
+}
+
+struct Place: View {
+    let location: Location
+
+    var body: some View {
+        VStack {
+            Image(location.country)
+                .renderingMode(.original)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+
+            Text(location.name)
+                .font(.title3)
+            Text(location.country)
+                .foregroundColor(.secondary)
+                .font(.subheadline)
+        }
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 25)
+                .fill(Color.white)
+        )
+        .shadow(color: Color.black.opacity(0.2), radius: 10)
+        .padding()
     }
 }
 
