53b217900f034111eb14fb8d1cc5b92a185a5848
OddMagnet <mibruenen@gmail.com>
2021-04-14 18:16:16 +0200

    Add example solution for expanding List

--- Journeys/Journeys/Views/TipsView.swift
+++ Journeys/Journeys/Views/TipsView.swift
@@ -7,15 +7,37 @@
 
 import SwiftUI
 
+struct StructuredTip: Identifiable, Decodable {
+    var id: String { content }
+    var content: String
+    var children: [StructuredTip]?
+}
+
 struct TipsView: View {
     let tips = Bundle.main.decode([Tip].self, from: "tips.json")
 
+    var flattenedTips: [StructuredTip] {
+        var returnValue = [StructuredTip]()
+
+        for tip in tips {
+            let child = StructuredTip(content: tip.body)
+            let parent = StructuredTip(content: tip.title, children: [child])
+            returnValue.append(parent)
+        }
+
+        return returnValue
+    }
+
     var body: some View {
-        List(tips) { tip in
+        // Example solution for expanding List
+        List(flattenedTips, children: \.children) { tip in
             VStack(alignment: .leading) {
-                Text(tip.title)
-                    .font(.headline)
-                Text(tip.body)
+                if tip.children != nil {
+                    Text(tip.content)
+                        .font(.headline)
+                } else {
+                    Text(tip.content)
+                }
             }
             .padding(.vertical)
         }
