d1cd3a1b682c0cfb5f85a7241a0eddfd3b630741
OddMagnet <mibruenen@gmail.com>
2021-04-20 16:14:33 +0200

    Add example solution for Color Pickers

--- NaNoGo/NaNoGo/ContentView.swift
+++ NaNoGo/NaNoGo/ContentView.swift
@@ -10,18 +10,76 @@ import SwiftUI
 struct ContentView: View {
     @SceneStorage("text") var text = ""
     @State private var fontSize: CGFloat = 17
+    @State private var backgroundColor: Color
+    @State private var foregroundColor: Color
+
+    init() {
+        let bg = UserDefaults.standard.color(forKey: "Background") ?? .white
+        let fg = UserDefaults.standard.color(forKey: "Foreground") ?? .black
+
+        _backgroundColor = State(wrappedValue: bg)
+        _foregroundColor = State(wrappedValue: fg)
+    }
 
     var body: some View {
         NavigationView {
             TextEditor(text: $text)
                 .font(.system(size: fontSize))
+                .foregroundColor(foregroundColor)
+                .background(backgroundColor)
                 .toolbar {
                     ToolbarItem(placement: .bottomBar) {
-                        Stepper("Font size", value: $fontSize)
-                            .labelsHidden()
+                        HStack {
+                            Stepper("Font size", value: $fontSize)
+                                .labelsHidden()
+                            ColorPicker("BG", selection: $backgroundColor)
+                                .fixedSize()
+                            ColorPicker("FG", selection: $foregroundColor)
+                                .fixedSize()
+                        }
                     }
                 }
                 .navigationTitle("NaNoGo")
+        }
+        .onAppear {
+            UITextView.appearance().backgroundColor = .clear
+        }
+        .onChange(of: backgroundColor) { value in
+            UserDefaults.standard.set(value, forKey: "Background")
+        }
+        .onChange(of: foregroundColor) { value in
+            UserDefaults.standard.set(value, forKey: "Foreground")
+        }
+    }
+}
+
+extension UserDefaults {
+    func set(_ value: Color, forKey key: String) {
+        let color = UIColor(value)
+
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+
+        color.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+
+        UserDefaults.standard.set(red, forKey: "\(key)Red")
+        UserDefaults.standard.set(green, forKey: "\(key)Green")
+        UserDefaults.standard.set(blue, forKey: "\(key)Blue")
+        UserDefaults.standard.set(alpha, forKey: "\(key)Alpha")
+        UserDefaults.standard.set(true, forKey: key)
+    }
+
+    func color(forKey key: String) -> Color? {
+        if UserDefaults.standard.bool(forKey: key) {
+            let red = UserDefaults.standard.double(forKey: "\(key)Red")
+            let green = UserDefaults.standard.double(forKey: "\(key)Green")
+            let blue = UserDefaults.standard.double(forKey: "\(key)Blue")
+            let alpha = UserDefaults.standard.double(forKey: "\(key)Alpha")
+            return Color(red: red, green: green, blue: blue, opacity: alpha)
+        } else {
+            return nil
         }
     }
 }
