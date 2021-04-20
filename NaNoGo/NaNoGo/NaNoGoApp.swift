d3629390c033baf6bb52f6b9f6fd2a35fbf3061e
OddMagnet <mibruenen@gmail.com>
2021-04-20 15:17:50 +0200

    Initial commit

---
+++ NaNoGo/NaNoGo/NaNoGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaNoGoApp.swift
+//  NaNoGo
+//
+//  Created by Michael Brünen on 20.04.21.
+//
+
+import SwiftUI
+
+@main
+struct NaNoGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
