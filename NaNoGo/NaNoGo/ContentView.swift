1a3ba7ccb8ece31a5eb08142d63022cb782d31f7
OddMagnet <mibruenen@gmail.com>
2021-04-20 15:22:53 +0200

    Add Text Editor

--- NaNoGo/NaNoGo/ContentView.swift
+++ NaNoGo/NaNoGo/ContentView.swift
@@ -8,9 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var text = ""
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            TextEditor(text: $text)
+                .font(.system(size: 16, weight: .semibold, design: .monospaced))
+                .navigationTitle("NaNoGo")
+        }
     }
 }
 
