709459d0ed7124698205226905bf2fb621f443a7
OddMagnet <mibruenen@gmail.com>
2021-04-20 15:40:18 +0200

    Add Toolbar for FontSize

--- NaNoGo/NaNoGo/ContentView.swift
+++ NaNoGo/NaNoGo/ContentView.swift
@@ -9,11 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     @SceneStorage("text") private var text = "programming..."
+    @State private var fontSize: CGFloat = 16
 
     var body: some View {
         NavigationView {
             TextEditor(text: $text)
-                .font(.system(size: 16, weight: .semibold, design: .monospaced))
+                .font(.system(size: fontSize, weight: .semibold, design: .monospaced))
+                .toolbar {
+                    ToolbarItem(placement: .bottomBar) {
+                        HStack {
+                            Stepper("\(fontSize, specifier: "%0.f")", value: $fontSize)
+                                .font(.title2)
+//                                .labelsHidden()   // needs a better way to inform user what stepper does
+                        }
+                    }
+                }
                 .navigationTitle("NaNoGo")
         }
         .navigationViewStyle(StackNavigationViewStyle())    // force iPadOS to give all the space to the editor
