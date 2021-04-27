b7728767648805da5ce7416e98e1710a25560c13
OddMagnet <mibruenen@gmail.com>
2021-04-27 21:28:09 +0200

    Configuration for ApplCip

---
+++ BarkingLot/BarkingClip/BarkingClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarkingClipApp.swift
+//  BarkingClip
+//
+//  Created by Michael Brünen on 27.04.21.
+//
+
+import SwiftUI
+
+@main
+struct BarkingClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
